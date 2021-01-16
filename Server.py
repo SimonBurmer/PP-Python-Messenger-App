430765766629164d5bedaae17b443818ce09883a
Simon <40640328+SimonBurmer@users.noreply.github.com>
2021-01-16 22:25:07 +0100

    evisions on server.py

--- Server.py
+++ Server.py
@@ -9,38 +9,42 @@ from threading import Thread
 def accept_incoming_connections():
     """Sets up handling for incoming clients."""
     while True:
-        client, client_address = SERVER.accept()
+        client, client_address = SERVER.accept() # Server accepts tcp connection
+
         print("%s:%s has connected." % client_address)
-        client.send(bytes("Greetings from the cave! Now type your name and press enter!", "utf8"))
+        info_message = "Type your name and press enter!"
+        client.send(bytes(info_message, "utf8"))
+
         addresses[client] = client_address
         Thread(target=handle_client, args=(client,)).start()
 
 
 def handle_client(client):  # Takes client socket as argument.
     """Handles a single client connection."""
+    name = client.recv(BUFSIZ).decode("utf8") # Server receives message from client
 
-    name = client.recv(BUFSIZ).decode("utf8")
-    welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
-    client.send(bytes(welcome, "utf8"))
-    msg = "%s has joined the chat!" % name
-    broadcast(bytes(msg, "utf8"))
+    welcome_message = 'Welcome %s! Type {quit} to exit.' % name
+    client.send(bytes(welcome_message, "utf8"))
+    join_message = "%s has joined the chat!" % name
+    broadcast(bytes(join_message, "utf8"))
+
     clients[client] = name
 
     while True:
-        msg = client.recv(BUFSIZ)
-        if msg != bytes("{quit}", "utf8"):
-            broadcast(msg, name+": ")
+        message = client.recv(BUFSIZ)
+        if message != bytes("{quit}", "utf8"):
+            broadcast(message, name+": ")
         else:
             client.send(bytes("{quit}", "utf8"))
             client.close()
             del clients[client]
-            broadcast(bytes("%s has left the chat." % name, "utf8"))
+            left_message = "%s has left the chat." % name
+            broadcast(bytes(left_message, "utf8"))
             break
 
 
 def broadcast(msg, prefix=""):  # prefix is for name identification.
     """Broadcasts a message to all the clients."""
-
     for sock in clients:
         sock.send(bytes(prefix, "utf8")+msg)
 
